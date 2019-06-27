ab134e2fa8110df48a3b8e613f1f247c357b0a2f
Florencia Nodar <fnodar@algeiba.com>
2019-06-26 21:15:11 -0300

    Agregué el button float para compartir la url

--- NodarFlorencia/app/src/main/java/com/example/fnodar/nodarflorencia/DescriptionActivity.java
+++ NodarFlorencia/app/src/main/java/com/example/fnodar/nodarflorencia/DescriptionActivity.java
@@ -1,14 +1,20 @@
 package com.example.fnodar.nodarflorencia;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-public class DescriptionActivity extends AppCompatActivity {
+import android.widget.ImageButton;
+
+public class DescriptionActivity extends AppCompatActivity implements ImageButton.OnClickListener {
 
     private WebView webView;
+    ImageButton fab;
+    Intent intent;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -20,6 +26,7 @@ public class DescriptionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        intent = getIntent();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.description_activity);
         String url = this.getIntent().getStringExtra("url");
@@ -29,6 +36,19 @@ public class DescriptionActivity extends AppCompatActivity {
         this.webView.loadUrl(url);
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+        fab = (ImageButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == fab.getId()){
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Quiero compartirte esta noticia: \n" + intent.getExtras().getString("url"));
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+        }
+    }
 }
